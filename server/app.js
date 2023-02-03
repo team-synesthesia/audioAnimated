bb9e23a37d4014318d440d9f3675866262587fb2
Robert West <robert.david.west@gmail.com>
2023-02-03 10:50:45 -0500

    clean up for deployment

--- server/app.js
+++ server/app.js
@@ -1,45 +1,46 @@
-const path = require('path')
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-module.exports = app
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+module.exports = app;
 
 // logging middleware
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
 // body parsing middleware
-app.use(express.json())
+app.use(express.json());
 
 // auth and api routes
-app.use('/auth', require('./auth'))
-app.use('/api', require('./api'))
+app.use("/auth", require("./auth"));
+app.use("/api", require("./api"));
 
-app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '..', 'public/react-prod.html')));
-app.get('/prod', (req, res)=> res.sendFile(path.join(__dirname, '..', 'react-client/build/index.html')));
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "..", "react-client/build/index.html"))
+);
 
 // static file-serving middleware
-app.use(express.static(path.join(__dirname, '..', 'public')))
-app.use(express.static(path.join(__dirname, '..', 'react-client/build')))
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(express.static(path.join(__dirname, "..", "react-client/build")));
 
 // any remaining requests with an extension (.js, .css, etc.) send 404
 app.use((req, res, next) => {
   if (path.extname(req.path).length) {
-    const err = new Error('Not found')
-    err.status = 404
-    next(err)
+    const err = new Error("Not found");
+    err.status = 404;
+    next(err);
   } else {
-    next()
+    next();
   }
-})
+});
 
 // sends index.html
-app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public/react-prod.html'));
-})
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "react-client/build/index.html"));
+});
 
 // error handling endware
 app.use((err, req, res, next) => {
-  console.error(err)
-  console.error(err.stack)
-  res.status(err.status || 500).send(err.message || 'Internal server error.')
-})
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || "Internal server error.");
+});
