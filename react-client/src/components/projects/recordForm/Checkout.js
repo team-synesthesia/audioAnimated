c276f689106879bbe586f1dbe82dc6415f31e04d
Robert West <robert.david.west@gmail.com>
2023-02-04 16:40:00 -0500

    PaymentForm.js -> RecordForm.js

--- react-client/src/components/projects/recordForm/Checkout.js
+++ react-client/src/components/projects/recordForm/Checkout.js
@@ -6,24 +6,11 @@ import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
 import Button from "@mui/material/Button";
-import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
-import SelectTracksForm from "./SelectTracksForm";
-import PaymentForm from "./PaymentForm";
-import Review from "./Review";
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
+import SelectTracksForm from "./SelectTracksForm";
+import RecordForm from "./RecordForm";
+import Review from "./Review";
 
 const steps = ["Select Tracks", "Record", "Add to your project"];
 
@@ -32,7 +19,7 @@ function getStepContent(step, availableFiles) {
     case 0:
       return <SelectTracksForm availableFiles={availableFiles} />;
     case 1:
-      return <PaymentForm />;
+      return <RecordForm />;
     case 2:
       return <Review />;
     default:
@@ -54,53 +41,52 @@ export default function Checkout({ availableFiles }) {
   return (
     <div>
       <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
-        <Paper
-          // variant="outlined"
+        {/* <Paper
+          variant="outlined"
           sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
-        >
-          <Typography component="h1" variant="h4" align="center">
-            Record
-          </Typography>
-          <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
-            {steps.map((label) => (
-              <Step key={label}>
-                <StepLabel>{label}</StepLabel>
-              </Step>
-            ))}
-          </Stepper>
-          {activeStep === steps.length ? (
-            <React.Fragment>
-              <Typography variant="h5" gutterBottom>
-                Thank you for your order.
-              </Typography>
-              <Typography variant="subtitle1">
-                Your order number is #2001539. We have emailed your order
-                confirmation, and will send you an update when your order has
-                shipped.
-              </Typography>
-            </React.Fragment>
-          ) : (
-            <React.Fragment>
-              {getStepContent(activeStep, availableFiles)}
-              <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-                {activeStep !== 0 && (
-                  <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
-                    Back
-                  </Button>
-                )}
-
-                <Button
-                  variant="contained"
-                  onClick={handleNext}
-                  sx={{ mt: 3, ml: 1 }}
-                >
-                  {activeStep === steps.length - 1 ? "Place order" : "Next"}
+        > */}
+        <Typography component="h1" variant="h4" align="center">
+          Record
+        </Typography>
+        <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
+          {steps.map((label) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+        {activeStep === steps.length ? (
+          <React.Fragment>
+            <Typography variant="h5" gutterBottom>
+              Thank you for your order.
+            </Typography>
+            <Typography variant="subtitle1">
+              Your order number is #2001539. We have emailed your order
+              confirmation, and will send you an update when your order has
+              shipped.
+            </Typography>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            {getStepContent(activeStep, availableFiles)}
+            <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+              {activeStep !== 0 && (
+                <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
+                  Back
                 </Button>
-              </Box>
-            </React.Fragment>
-          )}
-        </Paper>
-        <Copyright />
+              )}
+
+              <Button
+                variant="contained"
+                onClick={handleNext}
+                sx={{ mt: 3, ml: 1 }}
+              >
+                {activeStep === steps.length - 1 ? "Place order" : "Next"}
+              </Button>
+            </Box>
+          </React.Fragment>
+        )}
+        {/* </Paper> */}
       </Container>
     </div>
   );
