a0af13009213e0e969085bc398ca5ac82bea1ec7
Chris Jung <chris.cj.jung@gmail.com>
2023-02-09 15:52:24 -0500

    styled file upload form

--- react-client/src/components/projects/FileUploadForm.js
+++ react-client/src/components/projects/FileUploadForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 
-import { Button } from "@mui/material";
+import { Button, Box, Input, Typography } from "@mui/material";
+import FileUploadIcon from "@mui/icons-material/FileUpload";
 
 import { addFileAsync, writeFileAsync, getFileAsync } from "../../features";
 
@@ -28,35 +29,56 @@ const FileUploadForm = (props) => {
     dispatch(writeFileAsync({ projectId, filePath: file.name, file }));
     dispatch(getFileAsync({ fileLabel: name, projectId, filePath: file.name }));
 
-    const fileInput = document.querySelector("#fileInput");
-    fileInput.value = "";
     setName("");
     setFile({});
     handleClose();
   };
 
   return (
-    <div>
-      <form onSubmit={handleFormSubmit}>
-        <label htmlFor="name">File Label: </label>
-        <input
-          name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-        <label htmlFor="filePath">File: </label>
-        <input
-          id="fileInput"
-          type="file"
-          accept=".ogg, .mp3"
-          name="file"
-          onChange={(e) => setFile(e.target.files[0])}
-          required
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </div>
+    <form onSubmit={handleFormSubmit}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Box>
+          <label htmlFor="name">File Label: </label>
+          <Input
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+          <Button
+            color={file && file.name ? "primary" : "error"}
+            sx={{ margin: "5px" }}
+          >
+            <label htmlFor="filePath">
+              {file && file.name ? `${file.name}` : <FileUploadIcon />}
+            </label>
+          </Button>
+          <Input
+            id="filePath"
+            type="file"
+            accept=".ogg, .mp3"
+            name="file"
+            onChange={(e) => setFile(e.target.files[0])}
+            required
+            sx={{ display: "none" }}
+          />
+        </Box>
+        <Button
+          type="submit"
+          variant="contained"
+          sx={{ alignSelf: "flex-end" }}
+        >
+          <Typography>Submit</Typography>
+        </Button>
+      </Box>
+    </form>
   );
 };
 
