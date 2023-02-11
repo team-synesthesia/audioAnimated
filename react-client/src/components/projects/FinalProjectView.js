850f9f871e3bb85cf061f9d0f7502c6dc6cf5ba9
Chris Jung <chris.cj.jung@gmail.com>
2023-02-11 17:52:55 -0500

    created react route for share page

---
+++ react-client/src/components/projects/FinalProjectView.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { Box } from "@mui/material";
+
+const FinalProjectView = () => {
+  return (
+    <Box
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+        <div>asdf</div>
+    </Box>
+  );
+};
+
+export default FinalProjectView;
