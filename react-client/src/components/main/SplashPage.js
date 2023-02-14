7bd418ec058c636324e60c6e5b903dcdae16f81b
Chris Jung <chris.cj.jung@gmail.com>
2023-02-13 20:48:02 -0500

    changed spash page vid

--- react-client/src/components/main/SplashPage.js
+++ react-client/src/components/main/SplashPage.js
@@ -25,7 +25,7 @@ const SplashPage = (props) => {
             <iframe
               width="100%"
               height="100%"
-              src={"https://www.youtube-nocookie.com/embed/nFD03q4_A58"}
+              src={"https://www.youtube-nocookie.com/embed/h_EGWJua-0w"}
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
