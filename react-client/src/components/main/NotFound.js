987dcc316c34521e73b3841bbac896cb7e6cd01e
Chris Jung <chris.cj.jung@gmail.com>
2023-02-16 15:24:26 -0500

    finished 404 page

--- react-client/src/components/main/NotFound.js
+++ react-client/src/components/main/NotFound.js
@@ -8,11 +8,16 @@ const NotFound = () => {
       sx={{
         minHeight: "100vh",
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
       }}
     >
       <h1>ERROR: Page not found</h1>
+      <img
+        src="https://media.istockphoto.com/id/1278808623/vector/cat-sits-in-a-box-with-a-404-sign-page-or-file-not-found-connection-error.jpg?s=612x612&w=0&k=20&c=oQLypQ5q2p6-5o3-jvZzbPMxS2XPVt8_dCCnvBVL8R4="
+        alt="cat with 404 error sign"
+      />
     </Box>
   );
 };
